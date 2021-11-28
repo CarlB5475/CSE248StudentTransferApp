3f14fce12be7a5662602f9b94a3928ea36f9d0aa
James Nanas <jamescbn@yahoo.com>
2021-11-27 19:24:18 -0500

    Started work on College Search view and controller.

--- src/controllers/CollegeSearchController.java
+++ src/controllers/CollegeSearchController.java
@@ -1,13 +1,68 @@
 package controllers;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.*;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 import models.*;
 import utilities.*;
 
-public class CollegeSearchController {
+public class CollegeSearchController implements Initializable {
 	private ViewableStudent loggedStudent;
 	private CollegeSearchModel collegeSearchModel = new CollegeSearchModel(SQLiteConnection.getMainUrl());
 	
+	@FXML private Menu optionsMenu;
+	
+	@FXML private CheckBox zipCodeCheckBox; 
+	@FXML private CheckBox distanceCheckBox;
+	@FXML private CheckBox attendanceCostCheckBox;
+	@FXML private CheckBox collegeTypeCheckBox;
+	@FXML private CheckBox studentSizeCheckBox;
+	
+	@FXML private TextField radiusInput;
+	@FXML private TextField minCostInput;
+	@FXML private TextField maxCostInput;
+	@FXML private TextField minStudentSizeInput;
+	@FXML private TextField maxStudentSizeInput;
+	
+	@FXML private RadioButton publicRadioButton;
+	@FXML private RadioButton privateNonProfitRadioButton;
+	@FXML private RadioButton privateForProfitRadioButton;
+	private ToggleGroup collegeTypeToggleGroup;
+	
 	public void setStudent(ViewableStudent student) {
 		loggedStudent = student;
+	}
+	
+	public void viewStudentProfile(ActionEvent event) {
+		
+	}
+	
+	public void logOut(ActionEvent event) {
+		
+	}
+	
+	public void searchColleges(ActionEvent event) {
+		
+	}
+	
+	public void viewSelectedCollege(ActionEvent event) {
+		
+	}
+	
+	public void addFavoriteCollege(ActionEvent event) {
+		
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		collegeTypeToggleGroup = new ToggleGroup();
+		this.publicRadioButton.setToggleGroup(collegeTypeToggleGroup);
+		this.privateNonProfitRadioButton.setToggleGroup(collegeTypeToggleGroup);
+		this.privateForProfitRadioButton.setToggleGroup(collegeTypeToggleGroup);
+		
 	}
 }
